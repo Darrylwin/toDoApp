ea4d7670addb78456890187f875886fdf6269b3b
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-20 15:49:08 +0000

    ToDoTile créée

--- lib/components/to_do_tile.dart
+++ lib/components/to_do_tile.dart
@@ -1,27 +1,43 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class ToDoTile extends StatefulWidget {
-  const ToDoTile({super.key});
+class ToDoTile extends StatelessWidget {
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
-  @override
-  State<ToDoTile> createState() => _ToDoTileState();
-}
+  final String taskName;
 
-class _ToDoTileState extends State<ToDoTile> {
+  final bool? taskCompleted;
+
+  void Function(bool?)? onChanged;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        padding: EdgeInsets.all(12*2),
+        padding: EdgeInsets.all(12 * 2),
         decoration: BoxDecoration(
           color: Color(0xFF193540),
+          borderRadius: BorderRadius.circular(15),
         ),
-        child: Text(
-          "Personnalised Text",
-          style: TextStyle(
-            color: Colors.white70,
-          ),
+        child: Row(
+          children: [
+            // checkbox
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+
+            //task
+            Text(
+              taskName,
+              style: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+          ],
         ),
       ),
     );
